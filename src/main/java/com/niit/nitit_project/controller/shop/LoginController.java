5959d5dc8c641aff4a6a25a0d02f55f8aa5d9eac
tiengiddy123 <74064735+tiengiddy123@users.noreply.github.com>
2021-07-06 10:10:25 +0700

    Fix code

--- src/main/java/com/niit/nitit_project/controller/shop/LoginController.java
+++ src/main/java/com/niit/nitit_project/controller/shop/LoginController.java
@@ -2,11 +2,9 @@ package com.niit.nitit_project.controller.shop;
 
 import com.niit.nitit_project.model.dto.UserDTO;
 import com.niit.nitit_project.service.shop.LoginService;
-import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
